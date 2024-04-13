3ec40bacfce4d41a68a01488bced5a780d8fb07c
A.H.Mansouri <83764851+A-H-Mansoury@users.noreply.github.com>
2024-04-13 18:48:09 +0330

    Update ForwardKinematic.py

--- ForwardKinematic.py
+++ ForwardKinematic.py
@@ -4,7 +4,7 @@ def generate_H(theta, d, a, alpha):
     s = np.sin
     c = np.cos
     tmp = [
-        [c(theta), -s(theta)*c(alpha), -s(theta)*s(alpha), a*c(theta)],
+        [c(theta), -s(theta)*c(alpha), s(theta)*s(alpha), a*c(theta)],
         [s(theta), c(theta)*c(alpha), -c(theta)*s(alpha), a*s(theta)],
         [0, s(alpha), c(alpha), d],
         [0, 0, 0, 1]
